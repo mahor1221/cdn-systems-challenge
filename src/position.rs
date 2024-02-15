a67538f0322bb49142ae7e1d5a6ca33034ce0604
Mahor Foruzesh <mahor1221@gmail.com>
2024-02-15 13:12:35 +0330

    Add Position tests

--- src/position.rs
+++ src/position.rs
@@ -2,6 +2,7 @@ use crate::{
   error::{CdnError, CdnResult},
   world::WorldConfig,
 };
+use core::panic;
 use ndarray::{Dim, NdIndex};
 use rand::{
   distributions::{Distribution, Standard},
@@ -11,7 +12,7 @@ use rand::{
 };
 use std::{fmt::Debug, hash::Hash, marker::PhantomData};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum MoveDirection {
   Right,
   Left,
@@ -35,6 +36,10 @@ pub struct Position<C: WorldConfig> {
 
 impl<C: WorldConfig> Position<C> {
   pub fn new(x: usize, y: usize) -> Self {
+    if x >= C::MAX_LEN_X || y >= C::MAX_LEN_Y {
+      panic!("x and y must be smaller than MAX_X and MAX_Y")
+    }
+
     Self {
       inner: PositionInner { x, y },
       phantom: PhantomData,
@@ -42,12 +47,12 @@ impl<C: WorldConfig> Position<C> {
   }
 
   pub fn new_random_set(rng: &mut ThreadRng, len: usize) -> Vec<Self> {
-    let mut numbers: Vec<usize> = (0..C::MAX_X * C::MAX_Y).collect();
+    let mut numbers: Vec<usize> = (0..C::MAX_LEN_X * C::MAX_LEN_Y).collect();
     numbers.shuffle(rng);
     numbers.truncate(len);
     numbers
       .into_iter()
-      .map(|n| Self::new(n % C::MAX_X, n / C::MAX_X))
+      .map(|n| Self::new(n % C::MAX_LEN_X, n / C::MAX_LEN_X))
       .collect()
   }
 
@@ -57,13 +62,13 @@ impl<C: WorldConfig> Position<C> {
 
   pub fn r#move(&mut self, direction: MoveDirection) -> CdnResult<()> {
     match direction {
-      MoveDirection::Right if self.inner.x < C::MAX_X - 1 => {
+      MoveDirection::Right if self.inner.x < C::MAX_LEN_X - 1 => {
         self.inner.x += 1;
       }
       MoveDirection::Left if self.inner.x > 0 => {
         self.inner.x -= 1;
       }
-      MoveDirection::Up if self.inner.y < C::MAX_Y - 1 => {
+      MoveDirection::Up if self.inner.y < C::MAX_LEN_Y - 1 => {
         self.inner.y += 1;
       }
       MoveDirection::Down if self.inner.y > 0 => {
@@ -75,13 +80,13 @@ impl<C: WorldConfig> Position<C> {
   }
 
   pub fn direction_to(&self, other: &Self) -> MoveDirection {
-    if self.inner.x.checked_add(1) == Some(other.inner.x) {
+    if self.inner.x.checked_add(1) == Some(other.inner.x) && self.inner.y == other.inner.y {
       MoveDirection::Right
-    } else if self.inner.x.checked_sub(1) == Some(other.inner.x) {
+    } else if self.inner.x.checked_sub(1) == Some(other.inner.x) && self.inner.y == other.inner.y {
       MoveDirection::Left
-    } else if self.inner.y.checked_add(1) == Some(other.inner.y) {
+    } else if self.inner.y.checked_add(1) == Some(other.inner.y) && self.inner.x == other.inner.x {
       MoveDirection::Up
-    } else if self.inner.y.checked_sub(1) == Some(other.inner.y) {
+    } else if self.inner.y.checked_sub(1) == Some(other.inner.y) && self.inner.x == other.inner.x {
       MoveDirection::Down
     } else {
       panic!("self and other are not adjacent positions")
@@ -104,8 +109,8 @@ impl<C: WorldConfig> Distribution<Position<C>> for Standard {
   fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Position<C> {
     Position {
       inner: PositionInner {
-        x: rng.gen_range(0..C::MAX_X),
-        y: rng.gen_range(0..C::MAX_Y),
+        x: rng.gen_range(0..C::MAX_LEN_X),
+        y: rng.gen_range(0..C::MAX_LEN_Y),
       },
       phantom: PhantomData,
     }
@@ -171,5 +176,100 @@ unsafe impl<C: WorldConfig> NdIndex<Dim<[usize; 2]>> for &Position<C> {
 
   fn index_unchecked(&self, strides: &Dim<[usize; 2]>) -> isize {
     self.to_index().index_unchecked(strides)
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::{MoveDirection::*, Position};
+  use crate::world::{test::Tst, WorldConfig};
+  use ndarray::{array, Array2};
+  use rand::thread_rng;
+  use std::collections::HashSet;
+
+  const LEN: usize = Tst::MAX_LEN_X * Tst::MAX_LEN_Y;
+
+  #[test]
+  fn test_uniqueness_of_random_set() {
+    let mut rng = thread_rng();
+    let set: HashSet<_> = Position::<Tst>::new_random_set(&mut rng, LEN)
+      .into_iter()
+      .collect();
+    assert_eq!(LEN, set.len())
+  }
+
+  #[test]
+  fn test_position_to_index() {
+    let arr2: Array2<usize> = array![[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]];
+
+    let idx = Position::<Tst>::new(2, 1).to_index();
+    assert_eq!(6, arr2[idx]);
+
+    let idx = Position::<Tst>::new(1, 2).to_index();
+    assert_eq!(9, arr2[idx]);
+
+    let idx = Position::<Tst>::new(3, 2).to_index();
+    assert_eq!(11, arr2[idx]);
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_new_position_1() {
+    Position::<Tst>::new(0, 4);
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_new_position_2() {
+    Position::<Tst>::new(4, 0);
+  }
+
+  #[test]
+  fn test_move_position() {
+    let mut pos = Position::<Tst>::new(Tst::MAX_LEN_X - 1, Tst::MAX_LEN_Y - 1);
+    pos.r#move(Up).unwrap_err();
+    pos.r#move(Right).unwrap_err();
+
+    let mut pos = Position::<Tst>::new(0, 0);
+    pos.r#move(Left).unwrap_err();
+    pos.r#move(Down).unwrap_err();
+
+    pos.r#move(Right).unwrap();
+    assert_eq!([0, 1], pos.to_index());
+    pos.r#move(Left).unwrap();
+    assert_eq!([0, 0], pos.to_index());
+    pos.r#move(Up).unwrap();
+    assert_eq!([1, 0], pos.to_index());
+    pos.r#move(Down).unwrap();
+    assert_eq!([0, 0], pos.to_index());
+  }
+
+  #[test]
+  fn test_direction_to_position() {
+    let pos1 = Position::<Tst>::new(1, 1);
+
+    let pos2 = Position::<Tst>::new(2, 1);
+    assert_eq!(Right, pos1.direction_to(&pos2));
+    let pos2 = Position::<Tst>::new(1, 2);
+    assert_eq!(Up, pos1.direction_to(&pos2));
+    let pos2 = Position::<Tst>::new(0, 1);
+    assert_eq!(Left, pos1.direction_to(&pos2));
+    let pos2 = Position::<Tst>::new(1, 0);
+    assert_eq!(Down, pos1.direction_to(&pos2));
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_direction_to_non_adjacent_position_1() {
+    let pos = Position::<Tst>::new(0, 0);
+    pos.direction_to(&pos);
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_direction_to_non_adjacent_position_2() {
+    let pos1 = Position::<Tst>::new(0, 0);
+    let pos2 = Position::<Tst>::new(1, 1);
+    pos1.direction_to(&pos2);
   }
 }
